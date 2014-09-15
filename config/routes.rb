738a39c51d8334a1fe60f56067bce97768a4ec11
Eiko Seino <eikoseino@hotmail.com>
2014-09-15 15:24:53 -0700

    sets 'test_itmes', 'users', 'sessions' and root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :test_items
+  resource :users
+  resource :session, only: [:new, :create, :destroy]
+  root 'test_items#index' # shortcut for the above
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
